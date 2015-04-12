10ded2d173c8ab21c67818b5028d11573bb61c3d
Joey Robichaud <joe.robich@gmail.com>
2015-04-12 09:24:32 -0400

    Changed syntax theme to use overrides instead of replacing language files. Updated build script to also package a copy of the syntax fdz within the fdz.

--- build.ps1
+++ build.ps1
@@ -3,13 +3,23 @@ Add-Type -A 'System.IO.Compression.FileSystem';
 $currentDirectory = Get-Location;
 [IO.Directory]::SetCurrentDirectory($currentDirectory);
 
+#Make Build Directory
 Remove-Item Build -recurse
 New-Item Build -type directory
 
+#Create initial syntax theme fdz
 New-Item Build/VinkSyntaxTheme -type directory
 Copy-Item VinkSyntax/* Build/VinkSyntaxTheme -recurse
 [IO.Compression.ZipFile]::CreateFromDirectory('Build\VinkSyntaxTheme', 'Build\VinkSyntaxTheme.fdz');
+
+#Copy syntax theme so it will be deployed with other syntax themes
+New-Item 'Build/VinkSyntaxTheme/$(BaseDir)/Settings/Theme/SyntaxThemes' -type directory
+Move-Item Build/VinkSyntaxTheme.fdz 'Build/VinkSyntaxTheme/$(BaseDir)/Settings/Theme/SyntaxThemes/VinkSyntaxTheme.fdz'
+
+#Recreate syntax theme fdz
+[IO.Compression.ZipFile]::CreateFromDirectory('Build\VinkSyntaxTheme', 'Build\VinkSyntaxTheme.fdz');
 Remove-Item Build/VinkSyntaxTheme -recurse
 
+#Print hash for adding to appman.xml
 $hash = Get-FileHash Build/VinkSyntaxTheme.fdz -Algorithm MD5;
 Write-Host 'VinkSyntaxTheme.fdz hash: ' $hash.Hash;
